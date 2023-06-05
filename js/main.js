de3019e8f2fa56e61fff47aac5f31bb93248beec
Arūnas Mikalajūnas <mikalajunasar@gmail.com>
2023-06-05 19:21:02 +0300

    Fetch me

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
-import { data } from "../data/list.js";
+// import { data } from "../data/list.js";
 
 const ulDOM = document.querySelector('ul');
-
-const data = fetch('https://amik33.github.io/44-grupe-fetch/data/users.json')
+let HTML = '';
+const data = fetch ('https://amik33.github.io/44-grupe-fetch/data/users.json')
 .then((response) => {
     return response.json();
 })
 .then((data) => {
     for (const user of data) {
-        console.log(user);
+        HTML += `<ul class="service">
+        <il class="service-title">${user.name}</il>
+        <il>${user.age}</il>
+        <il class="service-description">${user.isMarried}</il>
+        </ul>`;
+        ulDOM.innerHTML = HTML;
     }
 });
 
-// let HTML = '';
 
 // for ( const item of data) {
-//     HTML += `<li>${item.id}) ${item.text}</li>`; 
+//     HTML += `<li>${item.id}) ${item.text}</li>`;
+//      ulDOM.innerHTML = HTML;
 // }
-//  ulDOM.innerHTML = HTML;
+
