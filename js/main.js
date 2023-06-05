9cd4b3e50e2a534b1cc37b1e162644f4cebfce9e
Arūnas Mikalajūnas <mikalajunasar@gmail.com>
2023-06-05 14:18:12 +0300

    JSON data

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 import { data } from "../data/list.js";
 
-console.log(data);
-
 const ulDOM = document.querySelector('ul');
+
+const data = fetch('http://127.0.0.1:4414/data/users.json')
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    for (const user of data) {
+        console.log(user);
+    }
+});
 
 let HTML = '';
 
