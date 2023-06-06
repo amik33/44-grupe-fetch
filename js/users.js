0a59c07e9dba4b0a96979efa04eeac8684b04943
Arūnas Mikalajūnas <mikalajunasar@gmail.com>
2023-06-06 15:35:36 +0300

    JS Doc

--- js/users.js
+++ js/users.js
@@ -1,3 +1,12 @@
+/**
+ * Pagal gautus vartotoju duomenis yra sugeneruojamas HTML.
+ * @param {Object[]} data Vartotoju duomenu sarasas.
+ * @param {string} data[].name Vartotojo vardas;
+ * @param {number} data[].age Vartotojo amzius metais;
+ * @param {boolean} data[].isMarried Vartotojo vedybinis statusas;
+ * @returns 
+ */
+
 export function renderUsersList(data) {
     const ulDOM = document.querySelector('ul');
     let HTML = '';
@@ -8,8 +17,17 @@ export function renderUsersList(data) {
                     <il>${user.age}</il>
                     <il class="service-description">${user.isMarried}</il>
                 </ul>`;
-
     }
         
    ulDOM.innerHTML = HTML;
 }
+
+/**
+ * Assign the project to a list of employees.
+ * @param {Object[]} employees - The employees who are responsible for the project.
+ * @param {string} employees[].name - The name of an employee.
+ * @param {string} employees[].department - The employee's department.
+ */
+// Project.prototype.assign = function (employees) {
+//     // ...
+// };
